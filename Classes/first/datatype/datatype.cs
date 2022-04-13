87f817411969a6cbe3899c4521f897d9ae1e65b5
Dhiraj <vc75it07@vedascollege.edu.np>
2022-04-13 12:02:54 +0545

    new

---
+++ Classes/first/datatype/datatype.cs
@@ -0,0 +1,17 @@
+﻿//DataTypes
+
+//Number: Integer, Floating Point
+byte a= 255;
+short b = 3345;
+int c = 5325353;
+long d = 34325135353535353;
+
+float e= 456.234f; //7-8 digit after .
+double f= 3546.34343563; //17-18 digit after .
+decimal g= 3245654.2165132165m; // 25-30 digit after .
+
+bool h = true;
+
+char i = 'F';
+String j="Dhiraj";
+object k = ""; // used for all data type
---
+++ Classes/first/first.cs
@@ -0,0 +1,13 @@
+using System;
+
+
+
+namespace ABD{
+    class First{
+        public static void Main(){ //Entry Point
+            Console.WriteLine("Hey, Guys! How are you :D?");
+        }
+        //Pascal Casing: ExecuteThisCode
+        //camel Casing: executeThisCode
+    }
+}
